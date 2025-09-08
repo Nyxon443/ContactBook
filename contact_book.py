d58ab55e42c4560c59598a75a2497ab0f768a3d7
nyxon <blackandshady204@gmail.com>
2025-09-08 15:34:11 +0200

    Update contact_book.py

--- contact_book.py
+++ contact_book.py
@@ -1,1 +1,8 @@
-
+def add_contact():
+  name = input("enter contact name: ")
+  phone = input("enter phone number: ")
+  #the contact.txt is automatically created "a" manually appends in the file
+  with open("contacts.txt" , "a") as file: 
+      file.write(f"{name} , {phone}\n")
+    print ("contact successfully added !!")
+    
